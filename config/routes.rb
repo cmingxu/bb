e4cec349d1174fcf364114e242cf520a948dcb6e
kevin xu <cming.xu@gmail.com>
2015-08-11 15:24:57 +0800

    [MOD] backend setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'players/index'
+
   devise_for :moderators
   namespace :api do
     resources :buildings
     resources :alliances
   end
+
+  resources :players
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
